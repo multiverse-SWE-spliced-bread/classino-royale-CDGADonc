c084f1ccd00594c2d0222a0eb4ae71eaeb65c624
David Clarke <david26th@gmail.com>
2022-08-02 16:04:53 +0100

    Initial Commit

--- easy/dice.js
+++ easy/dice.js
@@ -1,7 +1,41 @@
 // write your classes:
+class Die {
+    constructor(sides){
+        if(sides < 4 || sides > 20) {
+            throw new Error('sides must be between 4 and 20')
+        }
+        this.sides = sides;
+    }
+    roll(){
+  return Math.ceil(Math.random() * this.sides);
+    }
+}
+
+
+class Cup {
+    constructor(dice){
+        let arrayBuilder = [];
+        for (let i = 0; i< dice.length; i++){
+            arrayBuilder.push(new Die(dice[i]));
+        }
+        this.dice = arrayBuilder;
+    }
+    spill(){
+        let arrayBuilder = [];
+        for (let i = 0; i < this.dice.length; i++){
+            // console.log(this.dice[i].roll())
+            arrayBuilder.push(this.dice[i].roll())
+        }
+        return arrayBuilder;
+    }
+}
 
 
 
+const testCup = new Cup([4,6,10]);
+console.log(testCup.spill());
+// console.log(testCup.spill());
+// console.log(testDie.roll())
 // test your classes with console.log
 // to show they work as intended:
 
